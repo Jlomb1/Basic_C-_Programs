00db67ef5f11d9fcdf05a2b892453c31d6927069
Jlomb1 <127466981+Jlomb1@users.noreply.github.com>
2023-06-12 21:34:28 -0600

    Daily report assignment and variablesdatatypes practice

---
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DailyReportAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.WriteLine("Student Daily Report.");
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine(); //string for a name
+
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine(); //string for the course name 
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber);
+            int total = pageNum;
+            Console.WriteLine("You entered: " + total); //converting the string to an int to input into the console.readline()
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            string needHelp = Console.ReadLine();
+            bool helpStat = Convert.ToBoolean(needHelp);
+            bool status = helpStat;
+            Console.WriteLine("you answered: " + status); //converting the string to a boolean to input into the console.readline()
+
+            Console.WriteLine("Were there any positive experiences you\'d like to share? Please give specifics.");
+            string feedBack = Console.ReadLine(); //string for feed back
+
+            Console.WriteLine("Is there any other feedback you\'d like to provide? Please be specific.");
+            string feedBack2 = Console.ReadLine(); //string for feed back 2
+            Console.WriteLine("Thank you for your feed back!");
+
+            Console.WriteLine("How many hours did you study today?"); 
+            string studyHours = Console.ReadLine();
+            float hours = Convert.ToInt32(studyHours);
+            float studyTotal = hours;
+            Console.WriteLine("You entered: " + studyTotal); //converting the string to a float to input into the console.readline()
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine(); //end!
+        }
+    }
+}
---
+++ VariablesandDataTypes.cs/VariablesandDataTypes.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VariablesandDataTypes.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            // This is a program with an understanding of variables and data types
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char randomLetter = 'X';
+            char questionMark = '\u2103';
+
+            decimal moneyinBank = 100.5m; //'m' is for the compiler to understand for decimal type
+            double heightinCentimeters = 2.11204985;
+            float secondsLeft = 2.62f; //'f' for float type
+            short temperatureonMars = -341;
+
+            string myName = "Joe";
+
+
+            // Example of 'casting' or converting
+            int currentAge = 26;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+            
+        }
+    }
+}
