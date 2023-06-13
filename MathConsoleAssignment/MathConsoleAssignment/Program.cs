09d480a4ce1e39b6b544d903b0a8429bfb1bdaa7
Jlomb1 <127466981+Jlomb1@users.noreply.github.com>
2023-06-12 23:25:16 -0600

    Math Console Assignment and edits to VariablesandDataTypes

---
+++ MathConsoleAssignment/MathConsoleAssignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MathConsoleAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // This takes an input from the user, multiplies it by 50 and then prints the result in the console.
+            Console.WriteLine("Enter a number that you would like to have multiplied by 50.");
+            string multNumber = Console.ReadLine();
+            int multNum = Convert.ToInt32(multNumber);
+            int multTotal = multNum * 50;
+            Console.WriteLine("Your entered number multiplied by 50 is: " + multTotal);
+            Console.ReadLine();
+
+            // This takes an input from the user, adds it by 25 and then prints the result in the console.
+            Console.WriteLine("Enter a number that you would like to have added by 25.");
+            string addNumber = Console.ReadLine();
+            int addNum = Convert.ToInt32(addNumber);
+            int addTotal = addNum + 25;
+            Console.WriteLine("Your entered number plus 25 is: " + addTotal);
+            Console.ReadLine();
+
+            // This takes an input from the user, divides it by 12.5 and then prints the result in the console.
+            Console.WriteLine("Enter a number that you would like to have divided by 12.5.");
+            string divNumber = Console.ReadLine();
+            double divNum = Convert.ToDouble(divNumber);
+            double divTotal = divNum / 12.5;
+            Console.WriteLine("Your entered number divided by 12.5 is: " + divTotal);
+            Console.ReadLine();
+
+            // This takes an input from the user and check if it is greater than 50 (i.e true or false).
+            Console.WriteLine("Enter a number to see if its greater than 50.");
+            string greaterThanNumber = Console.ReadLine();
+            int greaterThanNum = Convert.ToInt32(greaterThanNumber);
+            bool greaterThanTotal = greaterThanNum > 50;
+            Console.WriteLine(greaterThanTotal);
+            Console.ReadLine();
+
+            // This takes an input from the user, divides it by 7, then prints the remainder to the console (i.e %).
+            Console.WriteLine("Enter a number. This will find the remainder of that number after being divided by 7.");
+            string modNumber = Console.ReadLine();
+            int modNum = Convert.ToInt32(modNumber);
+            int modTotal = modNum % 7;
+            Console.WriteLine("The remainder is: " + modTotal);
+            Console.ReadLine();
+
+        }
+    }
+}
--- VariablesandDataTypes.cs/VariablesandDataTypes.cs/Program.cs
+++ VariablesandDataTypes.cs/VariablesandDataTypes.cs/Program.cs
@@ -6,7 +6,7 @@ namespace VariablesandDataTypes.cs
     {
         static void Main()
         {
-            // This is a program with an understanding of variables and data types
+            ////////////////////////////////////////// This is a program with an understanding of variables and data types
             //Console.WriteLine("What is your name?");
             //string yourName = Console.ReadLine();
             //Console.WriteLine("Your name is: " + yourName);
@@ -19,29 +19,103 @@ namespace VariablesandDataTypes.cs
             //Console.WriteLine("Your favorite number plus 5 is: " + total);
             //Console.ReadLine();
 
-            bool isStudying = false;
-            byte hoursWorked = 42;
-            sbyte currentTemp = -23;
-            char randomLetter = 'X';
-            char questionMark = '\u2103';
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemp = -23;
+            //char randomLetter = 'X';
+            //char questionMark = '\u2103';
 
-            decimal moneyinBank = 100.5m; //'m' is for the compiler to understand for decimal type
-            double heightinCentimeters = 2.11204985;
-            float secondsLeft = 2.62f; //'f' for float type
-            short temperatureonMars = -341;
+            //decimal moneyinBank = 100.5m; //'m' is for the compiler to understand for decimal type
+            //double heightinCentimeters = 2.11204985;
+            //float secondsLeft = 2.62f; //'f' for float type
+            //short temperatureonMars = -341;
 
-            string myName = "Joe";
+            //string myName = "Joe";
 
 
             // Example of 'casting' or converting
-            int currentAge = 26;
-            string yearsOld = currentAge.ToString();
+            //int currentAge = 26;
+            //string yearsOld = currentAge.ToString();
 
-            bool isRaining = true;
-            string rainingStatus = Convert.ToString(isRaining);
-            Console.WriteLine(rainingStatus);
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+            //Console.WriteLine(rainingStatus);
+            //Console.ReadLine();
+
+            //int num1 = 5;
+            //int num2 = 20;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //int num3 = 30;
+            //int num4 = 40;
+            //int total2 = num3 - num4;
+            //Console.WriteLine(total2);
+            //Console.ReadLine();
+
+            //int num5 = 6;
+            //int num6 = 10;
+            //int total3 = num5 * num6;
+            //Console.WriteLine(total3);
+            //Console.ReadLine();
+
+            //int num7 = 16;
+            //int num8 = 2;
+            //int total4 = num7 / num8;
+            //Console.WriteLine(total4);
+            //Console.ReadLine();
+
+            //string fname = "john";
+            //string mname = "jacobs";
+            //string lname = "jinglehimerschmitt";
+            //Console.WriteLine(fname + mname + lname);
+            //Console.ReadLine();
+
+            //////////////////////////////////////////////////////////// Math and comparison operators
+
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus ten = " + total.ToString());
+            //Console.ReadLine();
+
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+
+            //double quotion = 100.0 / 17.0;
+            //Console.WriteLine(quotion);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            //bool isWarm = currentTemp <= roomTemp;
+            bool isWarm = currentTemp == roomTemp; //OR '!=' which is NOT EQUAL
+
+            Console.WriteLine(isWarm);
             Console.ReadLine();
-            
+
+
+
+
         }
     }
 }
