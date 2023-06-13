a8236929f6de7d9217d4c639e3e901354ca140ae
Jlomb1 <127466981+Jlomb1@users.noreply.github.com>
2023-06-12 18:58:51 -0600

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World! If your even a \"world\" at all?");
+        Console.ReadLine();
+    }
+}
+
---
+++ WelcomeScreenTest.cs/WelcomeScreenTest.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeScreenTest.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we\'re \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
