1434fc6081a1941c1a9c6a814f1f9562463488d1
Jlomb1 <127466981+Jlomb1@users.noreply.github.com>
2023-06-26 15:18:00 -0600

    MethodClassAssignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class MathClass
+    {
+        public void MathOp(int data = 25, int data2 = 62) // "Void" indicates to NOT returning data. If "int" were in its place
+                                                          // the class would then be returning an int, but void means nothing is returned.
+        {
+            var result = data * 4;
+            Console.WriteLine(data2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathOperation = new MathClass(); // Instantiate an instants of MathClass.
+
+            int num1 = Convert.ToInt32(Console.ReadLine()); // Get two int from user.
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            mathOperation.MathOp(num1, num2); // Call MathOp method in the MathClass w/ user input num1 and num2.
+            mathOperation.MathOp(data: num1, data2: num2); // call method again specifying the "parameter" names.
+            Console.ReadLine(); 
+        }
+    }
+}
